b8600a65fa4a240d845844357e909b5a086bd458
LinMingjie <code@linmj.com>
2016-06-17 21:11:27 +0800

    update database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
 
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 # to avoid the circular import error, this import statement is at the end
 from app import views, models
