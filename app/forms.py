cab6d49dc1a6fb7d20904497c8aa295e6314465f
LinMingjie <code@linmj.com>
2016-06-20 20:11:43 +0800

    Implemented profile page

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, BooleanField,SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, InputRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from .models import User
+
+
+class EditForm(Form):
+    nickname = StringField('nickname', validators=[InputRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
 
 
 class LoginForm(Form):
